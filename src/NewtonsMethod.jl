871e3763a8a09e5075cc56d63b0ac17b95516c9f
Tsuyoshi Matsuura <tsuyoshi.matsuura@sigmaenergy.nl>
2025-06-07 17:09:13 +0200

    Initial commit

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,32 @@
 module NewtonsMethod
 
-# Write your package code here.
+using ForwardDiff, LinearAlgebra
+
+function newtonroot(f, f′, x0; tol = 1E-7, maxiter = 1000)
+    # setup the algorithm
+    x_old = x0
+    normdiff = Inf
+    iter = 1
+    while normdiff > tol && iter <= maxiter
+        x_new = x_old - f(x_old)/f′(x_old)   # use the passed in map
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter = iter + 1
+    end
+    if normdiff > tol
+        println("Solution not found")
+        return nothing
+    else
+        return (; value = x_old, normdiff, iter) # A named tuple
+    end
+end
+
+function newtonroot(f, x0; tol = 1E-7, maxiter = 1000)
+    D(f) = x -> ForwardDiff.derivative(f, x)
+    f′ = D(f)
+    newtonroot(f, f′, x0; tol = tol, maxiter = maxiter)
+end
+
+export newtonroot
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,32 @@ using NewtonsMethod
 using Test
 
 @testset "NewtonsMethod.jl" begin
-    # Write your tests here.
+    f(x) = 3*x^2 - 2*x -1
+    f′(x) = 6*x - 2
+    root = newtonroot(f, f′, -2/3)
+    @test root.normdiff < 1e-7
+
+    root = newtonroot(f, -2/3)
+    @test root.normdiff < 1e-7
+
+    f(x) = 3*x^2 - 2*x -1
+    f′(x) = 6*x - 2
+    root = newtonroot(f, f′, -2/3; maxiter=3)
+    @test root == nothing
+
+    f(x) = (x-1.0)^3
+    f′(x) = 3*(x-1.0)^2
+    root = newtonroot(f,f′,1.1)
+    @test root.normdiff < 1e-7
+
+    root = newtonroot(f,1.1)
+    @test root.normdiff < 1e-7
+
+    f(x) = 2 + x^2
+    f′(x) = 2*x
+    root = newtonroot(f,f′,0.1)
+    @test root == nothing
+
+    root = newtonroot(f,0.1)
+    @test root == nothing  
 end
